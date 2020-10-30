8e04596f0c1e6f2fcd1165e34fb9b7d396ada36c
Case Larsen <clarsen@gmail.com>
2020-10-29 21:58:05 -0700

    make AQI color on LCD, log to influx at 15s intervals

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <Arduino.h>
-#include <PrintEx.h>
 
 #include <TFT_eSPI.h>
 #include <SPI.h>
 
+#include <WiFi.h>
+#include <InfluxDbClient.h>
+
 #include "Free_Fonts.h" // Include the header file attached to this sketch
+#include "config.h"
 
 TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
@@ -13,8 +16,14 @@ const int PM_TX_GPIO = 17;
 
 /* TX on PM5003 which we receive from */
 const int PM_RX_GPIO = 16;
-PrintEx serialPrint = Serial;
 
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+
+InfluxDBClient influxClient;
+unsigned long influxReportingTime;
+
+void setup_wifi();
 void setup_display();
 void testdrawtext(const char *text, uint16_t color);
 void tftPrintTest();
@@ -23,7 +32,24 @@ void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  serialPrint.println("Starting.");
+  Serial.println("Starting.");
+
+  setup_wifi();
+  configTzTime(LOCAL_TZ, "pool.ntp.org", "time.nis.gov");
+
+  influxClient.setConnectionParamsV1(INFLUX_URL, INFLUX_DB, INFLUX_USER, INFLUX_PASSWORD);
+
+  if (influxClient.validateConnection())
+  {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(influxClient.getServerUrl());
+  }
+  else
+  {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(influxClient.getLastErrorMessage());
+  }
+
   setup_display();
   Serial2.begin(9600);
   Serial2.setTimeout(250);
@@ -64,7 +90,9 @@ void update_display(float pm2_5_aqi, float pm10_aqi, unsigned int pm2_5_color, u
   }
   int32_t ypos = 20 + 2 * tft.fontHeight(GFXFF);
   int width = 2 * tft.textWidth("M");
-  serialPrint.printf(" ypos %d, tft width %d, box width %d, box height %d\n", ypos, tft.width(), width, tft.fontHeight(GFXFF));
+  char str[128];
+  snprintf(str, sizeof(str), " ypos %d, tft width %d, box width %d, box height %d", ypos, tft.width(), width, tft.fontHeight(GFXFF));
+  Serial.println(str);
   tft.fillRect(tft.width() - width, ypos, width, ypos + tft.fontHeight(GFXFF), overall);
 }
 
@@ -110,23 +138,23 @@ struct aqi_entry
 };
 
 struct aqi_entry pm2_5_lut[] = {
-    {0, 50, 0.0, 12.1, TFT_COLOR(0, 57, 0)},        // green
-    {51, 100, 12.1, 35.5, TFT_COLOR(32, 64, 0)},    // yellow
-    {101, 150, 35.5, 55.5, TFT_COLOR(32, 32, 0)},   // orange
-    {151, 200, 55.5, 150.5, TFT_COLOR(32, 0, 0)},   // red
+    {0, 50, 0.0, 12.1, TFT_COLOR(0, 56, 0)},        // green
+    {51, 100, 12.1, 35.5, TFT_COLOR(31, 63, 0)},    // yellow
+    {101, 150, 35.5, 55.5, TFT_COLOR(31, 31, 0)},   // orange
+    {151, 200, 55.5, 150.5, TFT_COLOR(31, 0, 0)},   // red
     {201, 300, 150.5, 250.5, TFT_COLOR(18, 16, 8)}, // purple
-    {301, 400, 250.5, 350.5, TFT_COLOR(16, 0, 4)},  // maroon
-    {401, 500, 350.5, 500.5, TFT_COLOR(16, 0, 4)},  // maroon
+    {301, 400, 250.5, 350.5, TFT_COLOR(17, 0, 4)},  // maroon
+    {401, 500, 350.5, 500.5, TFT_COLOR(17, 0, 4)},  // maroon
 };
 
 struct aqi_entry pm10_lut[] = {
-    {0, 50, 0.0, 55, TFT_COLOR(0, 57, 0)},
-    {51, 100, 55, 155, TFT_COLOR(32, (64), 0)},
-    {101, 150, 155, 255, TFT_COLOR(32, 32, 0)},
-    {151, 200, 255, 355, TFT_COLOR(32, 0, 0)},
+    {0, 50, 0.0, 55, TFT_COLOR(0, 56, 0)},
+    {51, 100, 55, 155, TFT_COLOR(31, 63, 0)},
+    {101, 150, 155, 255, TFT_COLOR(31, 31, 0)},
+    {151, 200, 255, 355, TFT_COLOR(31, 0, 0)},
     {201, 300, 355, 425, TFT_COLOR(18, 16, 8)}, // purple
-    {301, 400, 425, 505, TFT_COLOR(16, 0, 4)},
-    {401, 500, 505, 605, TFT_COLOR(16, 0, 4)},
+    {301, 400, 425, 505, TFT_COLOR(17, 0, 4)},
+    {401, 500, 505, 605, TFT_COLOR(17, 0, 4)},
 };
 
 float aqi_for_pm(struct aqi_entry *lut, int n_lut, uint16_t pmcount, unsigned int *aqi_color)
@@ -190,23 +218,74 @@ void loop()
   {
     for (size_t i = 0; i < n; i++)
     {
-      serialPrint.printf("%02x:", buf[i]);
+      char str[8];
+      snprintf(str, sizeof(str), "%02x:", buf[i]);
+      Serial.print(str);
     }
-    serialPrint.printf(" %d read\n", n);
+    char str[12];
+    snprintf(str, sizeof(str), " %d read", n);
+    Serial.println(str);
+
     struct pms_sample s;
     if (decode(buf, &s))
     {
       unsigned int pm2_5_color, pm10_color;
       float pm2_5_aqi = aqi_for_pm(pm2_5_lut, sizeof(pm2_5_lut) / sizeof(pm2_5_lut[0]), s.atmospheric.pm2_5_ugm3, &pm2_5_color);
       float pm10_aqi = aqi_for_pm(pm10_lut, sizeof(pm10_lut) / sizeof(pm10_lut[0]), s.atmospheric.pm10_ugm3, &pm10_color);
-      serialPrint.printf("AQI for pm2.5 %0.2f\nAQI for pm10 %0.2f\nstd pm1.0 %d, pm2.5 %d, pm10 %d\natm pm1.0 %d, pm2.5 %d, pm10 %d\n>0.3 %d, 0.5 %d, >1.0 %d, >2.5 %d, >5.0 %d, >10.0 %d\n",
-                         // LRAPA AQI %0.2f\n 0.5 * s.atmospheric.pm2_5_ugm3 - 0.66,
-                         pm2_5_aqi,
-                         pm10_aqi,
-                         s.standard.pm1_0_ugm3, s.standard.pm2_5_ugm3, s.standard.pm10_ugm3,
-                         s.atmospheric.pm1_0_ugm3, s.atmospheric.pm2_5_ugm3, s.atmospheric.pm10_ugm3,
-                         s.bins.gt0_3um_dl, s.bins.gt0_5um_dl, s.bins.gt1um_dl, s.bins.gt2_5um_dl, s.bins.gt5um_dl, s.bins.gt10um_dl);
+      char str[128];
+      snprintf(str, sizeof(str), "AQI for pm2.5 %0.2f\nAQI for pm10 %0.2f\nstd pm1.0 %d, pm2.5 %d, pm10 %d\natm pm1.0 %d, pm2.5 %d, pm10 %d\n>0.3 %d, 0.5 %d, >1.0 %d, >2.5 %d, >5.0 %d, >10.0 %d",
+               // LRAPA AQI %0.2f\n 0.5 * s.atmospheric.pm2_5_ugm3 - 0.66,
+               pm2_5_aqi,
+               pm10_aqi,
+               s.standard.pm1_0_ugm3, s.standard.pm2_5_ugm3, s.standard.pm10_ugm3,
+               s.atmospheric.pm1_0_ugm3, s.atmospheric.pm2_5_ugm3, s.atmospheric.pm10_ugm3,
+               s.bins.gt0_3um_dl, s.bins.gt0_5um_dl, s.bins.gt1um_dl, s.bins.gt2_5um_dl, s.bins.gt5um_dl, s.bins.gt10um_dl);
+      Serial.println(str);
       update_display(pm2_5_aqi, pm10_aqi, pm2_5_color, pm10_color);
+      if (millis() > influxReportingTime)
+      {
+        Serial.println("logging to influx");
+        Point measurement("particulates");
+        measurement.addField("pm2_5_lrapa_aqi", pm2_5_aqi);
+        measurement.addField("pm10_lrapa_aqi", pm10_aqi);
+        measurement.addField("std_1_0_ugm3", s.standard.pm1_0_ugm3);
+        measurement.addField("std_2_5_ugm3", s.standard.pm2_5_ugm3);
+        measurement.addField("std_10_ugm3", s.standard.pm10_ugm3);
+        measurement.addField("atm_1_0_ugm3", s.atmospheric.pm1_0_ugm3);
+        measurement.addField("atm_2_5_ugm3", s.atmospheric.pm2_5_ugm3);
+        measurement.addField("atm_10_ugm3", s.atmospheric.pm10_ugm3);
+        measurement.addField("bins_gt0_3_um_dl", s.bins.gt0_3um_dl);
+        measurement.addField("bins_gt0_5_um_dl", s.bins.gt0_5um_dl);
+        measurement.addField("bins_gt1_um_dl", s.bins.gt1um_dl);
+        measurement.addField("bins_gt2_5_um_dl", s.bins.gt2_5um_dl);
+        measurement.addField("bins_gt5_um_dl", s.bins.gt5um_dl);
+        measurement.addField("bins_gt10_um_dl", s.bins.gt10um_dl);
+        measurement.addTag("location", "living room");
+        influxClient.writePoint(measurement);
+        influxReportingTime += 15000;
+      }
     }
   }
+}
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
